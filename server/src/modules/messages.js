3330c05def435c65744119bb31fb5053d8846ec9
Marwanrizik91 <marwan.rizik@gmail.com>
2020-07-27 21:51:34 +0300

    add to queries closes #4, add gitignore

--- server/src/modules/messages.js
+++ server/src/modules/messages.js
@@ -9,12 +9,16 @@ exports.getSentMessages = userId => {
 }
 
 exports.deleteMessage = async (messageId, userId) => {
-    const [row] = await db.query(`SELECT * from messages where id = $1`, [messageId])
-    if(row.senderId === userId) {
+    const [queryRes] = await db.query(`SELECT * from messages where id = $1`, [messageId])
+    if(queryRes.senderId === userId) {
         await db.query(`UPDATE messages SET "senderDeleted" = true where id = $1`, [messageId])
-    } else if(row.receiverId === userId){
+    } else if(queryRes.receiverId === userId){
         await db.query(`UPDATE messages SET "receiverDeleted" = true where id = $1`, [messageId])
     } else {
         throw new Error('No user exists with this id');
     }
 }
+
+exports.markAsRead =  messageId => db.query('UPDATE messages SET "isRead" = true where id = $1', [messageId])
+
+
