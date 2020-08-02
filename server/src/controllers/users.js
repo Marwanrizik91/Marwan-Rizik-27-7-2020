285aa310b8d65cd7951f7c4815038c4ff5571c73
Marwanrizik91 <marwan.rizik@gmail.com>
2020-08-02 15:38:32 +0300

    change cookie sameSite to none

--- server/src/controllers/users.js
+++ server/src/controllers/users.js
@@ -53,7 +53,7 @@ exports.login = async (req, res) => {
             throw new Error('incorrect password')
 
         const accessToken = generateAccessToken((userData.id).toString())
-        res.cookie('access_token', accessToken)
+        res.cookie('access_token', accessToken, { sameSite: "None"})
         res.json({ data: userData, message: 'Logged successfully', code: 200 })
 
     } catch ({ message }) {
